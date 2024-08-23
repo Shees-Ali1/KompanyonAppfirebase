f4e2a0e0057f8e8d06092ccf87fbf16a20adc6c1
Hamzair <152982499+Hamzair@users.noreply.github.com>
2024-08-23 16:21:26 +0500

    first commit...

---
+++ lib/controller/signup_contoller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignupContoller extends GetxController {
+
+
+}
